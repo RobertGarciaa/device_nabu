cfdca2b5e9170888c832f32476399c06f0518820
RobertGarciaa <chae0218@naver.com>
2024-11-19 17:19:41 +0900

    nabu: Enable BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES
    Signed-off-by: RobertGarciaa <chae0218@naver.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,6 +10,7 @@ include device/xiaomi/sm8150-common/BoardConfigCommon.mk
 DEVICE_PATH := device/xiaomi/nabu
 
 BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # Display
 TARGET_SCREEN_DENSITY := 360
